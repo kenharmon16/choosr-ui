54585f0aa07787ddc38ccd899de59852c2ac9a83
Ken Harmon <kenharmon16@gmail.com>
2026-05-03 12:22:35 -0500

    Adding privacy policy to repo

--- app.config.js
+++ app.config.js
@@ -3,6 +3,6 @@ module.exports = ({ config }) => ({
   ...config,
   extra: {
     ...config.extra,
-    eas: { ...(config.extra?.eas || {}), projectId: process.env.EAS_PROJECT_ID },
+    eas: { ...(config.extra?.eas || {}), projectId: "cb326385-0d3e-47e6-af70-5fd5080bf156" },
   },
 });
